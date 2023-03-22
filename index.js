9df83783dd146c7def67fcf9220c7cff3a6e4852
K1ngVlad <kingvlad3008@gmail.com>
2023-03-22 18:56:38 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const img = document.querySelector('img');
+const text = document.querySelector('.text');
+
+const letsbirthday = () => {
+  img.setAttribute('src', '/assets/dr.gif');
+  setTimeout(() => {
+    img.style.display = 'none';
+    text.classList.add('animate');
+  }, 3500);
+};
+
+img.addEventListener('click', letsbirthday);
+
+document.addEventListener('mousemove', (e) => {
+  const width = document.documentElement.clientWidth;
+  const height = document.documentElement.clientHeight;
+  const x = e.clientX;
+  const y = e.clientY;
+  console.log(x, y);
+  const obj = document.querySelector('.animate');
+  if (obj) {
+    obj.style.transform = `translate(${(width - x) / 25}px , ${
+      (height - y) / 25
+    }px)`;
+  }
+});
